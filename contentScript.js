e28c56e22afc9079272291fe4b4d517375b51a7e
Vedant Agarwala <mail@vedant-agarwala.com>
2018-12-05 23:23:25 +0530

    Identify DOM elements with phone numbers

--- contentScript.js
+++ contentScript.js
@@ -1,25 +1,36 @@
-console.log("yay");
-
-function getPhoneNumbers(libPhoneNumber, document) {
+function getPhoneNumbers(document, libPhoneNumber) {
   // Use the non-standard innerText which has some caveats:
   // https://stackoverflow.com/questions/35213147/difference-between-textcontent-vs-innertext
   const visibleText = document.body.innerText;
-  const numberObjects = libPhoneNumber.findNumbers(visibleText, { v2: true })
-  return numberObjects.map(phoneNumber => phoneNumber.number.number.substring(1))
+
+  return libPhoneNumber.findNumbers(visibleText, { v2: true });
 }
 
 /**
  * Element texts are matched against formattedPhoneNumbers
- * @param formattedPhoneNumbers Array of phone numbers that can be understood by WhatsApp
+ * @param phoneNumbers Array of phone numbers that can be understood by WhatsApp
  */
-function addSendToWaButtons(document, formattedPhoneNumbers) {
+function addSendToWaButtons(element, phoneNumbers, libPhoneNumber) {
+  element.childNodes.forEach(function (child) {
+    addSendToWaButtons(child, phoneNumbers);
+  });
 
+  if (waNumber = matchingWhatsAppNumber(element, phoneNumbers, libPhoneNumber)) {
+    console.log(element);
+  }
 }
 
+function matchingWhatsAppNumber(element, phoneNumbers, libPhoneNumber) {
+  for (phoneNumber of phoneNumbers) {
+    if (phoneNumber.number.nationalNumber == element.textContent) {
+      return phoneNumber.number.number.substring(1);
+    }
+  }
+}
 
 function main(libPhoneNumber) {
-  const phoneNumbers = getPhoneNumbers(libPhoneNumber, document);
-  console.log(phoneNumbers);
+  const phoneNumbers = getPhoneNumbers(document, libPhoneNumber);
+  addSendToWaButtons(document.body, phoneNumbers, libPhoneNumber);
 }
 
 // ref: https://requirejs.org/docs/errors.html#notloaded
