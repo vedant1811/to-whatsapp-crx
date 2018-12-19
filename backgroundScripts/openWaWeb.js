415b7ce25f908cbda731230d64ea3fb3bffd3eaf
Vedant Agarwala <vedant.agarwala@grabtaxi.com>
2018-12-20 01:45:02 +0800

    Add a incomplete autoSendingTo

--- backgroundScripts/openWaWeb.js
+++ backgroundScripts/openWaWeb.js
@@ -1,3 +1,5 @@
+var autoSendingTo = null;
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log('onMessage');
@@ -14,19 +16,20 @@ function createWaTab(data, sender) {
   chrome.tabs.create({
     url: createWaWebLink(data.waNumber, data.message),
     active: !autoSend
-  }, createdTab => sendMessageOnWaTab(createdTab, autoSend))
+  }, createdTab => sendMessageOnWaTab(createdTab, data.waNumber, autoSend))
 }
 
 function onWaTabResponse(data, sender) {
 
 }
 
-function sendMessageOnWaTab(waTab, autoSend = true) {
+function sendMessageOnWaTab(waTab, waNumber, autoSend = true) {
   console.log(`'sendMessageOnWaTab', autoSend: ${autoSend}`);
   console.log(waTab);
 
   if (autoSend) {
     chrome.tabs.executeScript(waTab.id, { file: 'contentScripts/waSendInBackground.js' });
+    autoSendingTo = waNumber;
   }
 }
 
